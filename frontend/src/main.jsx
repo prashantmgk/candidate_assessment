19d04bcc12254f6e9dab1c3d7d2585e899e3ea05
prashantmgk <prashantmgk@gmail.com>
2026-07-13 23:48:09 +0545

    feat: add routing and authentication with react-router-dom and react-hook-form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './api/queryClient.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
