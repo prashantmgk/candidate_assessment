2fa8d7ce2931c32e7820b297d1b3dceb14c1ce47
prashantmgk <prashantmgk@gmail.com>
2026-07-13 23:46:20 +0545

    feat: add candidate and authentication pages for user navigation

---
+++ frontend/src/pages/CandidateDetailPage.jsx
@@ -0,0 +1,5 @@
+export default function CandidateDetailPage() {
+  return (
+    <p>Candidate Detail Page</p>
+  )
+}
---
+++ frontend/src/pages/CandidateListPage.jsx
@@ -0,0 +1,5 @@
+export default function CandidateListPage() {
+  return (
+    <p>Candidate List Page</p>
+  )
+}
